353b4a98182a98e64de055ec46c765be20530c47
JenPhD <jpemberton76@gmail.com>
2016-10-18 16:57:06 -0400

    add sync models to server.js

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ var users_controller = require('./controllers/users_controller');
 
 // instantiate our app
 var app = express();
+var Users = require('./models')['user'];
+var sequelBurgers = require('./models')['sequelburger'];
+Users.sync();
+sequelBurgers.sync();
 
 // override POST to have DELETE and PUT
 //Commenting out to see if it is affecting moving burgers to the devoured column.
